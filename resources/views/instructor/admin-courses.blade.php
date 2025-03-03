850d06de6085504991e2999417a4c9ca17f06ae5
pyramakerz-soft <pyramakerzgit@gmail.com>
2025-03-03 09:08:42 +0200

    Create course link

--- resources/views/instructor/admin-courses.blade.php
+++ resources/views/instructor/admin-courses.blade.php
@@ -48,7 +48,7 @@
                                 </div> --}}
                                 <div class="dashboardarea__right">
                                     <div class="dashboardarea__right__button">
-                                        <a class="default__button" href="/create-course">Create a New Course
+                                        <a class="default__button" href="/courses/create">Create a New Course
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                                 viewBox="0 0 24 24" fill="none" stroke="currentColor"
                                                 stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
