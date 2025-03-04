3a585bf3a838ba7e2206f6a7f843ba398b4c6f3f
pyramakerz-soft <pyramakerzgit@gmail.com>
2025-03-04 02:10:59 +0200

    Routes

--- resources/views/dashboard/admin-courses.blade.php
+++ resources/views/dashboard/admin-courses.blade.php
@@ -51,9 +51,10 @@
                                     </svg>
                                     <span>4.0 (120 Reviews)</span>
                                 </div> --}}
+                                @if(Auth::guard('admin')->user()->roles[0]->name == 'admin')
                                     <div class="dashboardarea__right">
                                         <div class="dashboardarea__right__button">
-                                            <a class="default__button" href="/create-course">Create a New Course
+                                            <a class="default__button" href="{{route('courses.create')}}">Create a New Course
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                                     viewBox="0 0 24 24" fill="none" stroke="currentColor"
                                                     stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
@@ -64,7 +65,7 @@
                                                 </svg></a>
                                         </div>
                                     </div>
-
+@endif
                                 </div>
                             </div>
                         </div>
