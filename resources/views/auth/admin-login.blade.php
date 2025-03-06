9526d059772f91d6e907f560aef622a9b88a36c2
pyramakerz-soft <pyramakerzgit@gmail.com>
2025-03-06 12:44:42 +0200

    edt

--- resources/views/auth/admin-login.blade.php
+++ resources/views/auth/admin-login.blade.php
@@ -83,7 +83,7 @@
                             </li>
                             <li class="nav-item" role="presentation">
                                 <button class="single__tab__link" data-bs-toggle="tab" data-bs-target="#projects__two"
-                                    type="button">sign up</button>
+                                    type="button">Signup</button>
                             </li>
                         </ul>
                     </div>
@@ -97,10 +97,8 @@
                                 <div class="loginarea__wraper">
                                     <div class="login__heading">
                                         <h5 class="login__title">Login</h5>
-                                        <p class="login__description">Don't have an account yet? <a href="#" class="single__tab__link"
-                                            data-bs-toggle="tab"
-                                            data-bs-target="#projects__two">Sign up for
-                                                free</a></p>
+                                        <p class="login__description">Don't have an account yet? | Sign up for
+                                                free</p>
                                     </div>
 
 
@@ -155,9 +153,7 @@
                                 <div class="loginarea__wraper">
                                     <div class="login__heading">
                                         <h5 class="login__title">sign up</h5>
-                                        <p class="login__description">Already have an account? <a href="#" class="single__tab__link"
-                                            data-bs-toggle="tab"
-                                            data-bs-target="#projects__one">Log In</a></p>
+                                        <p class="login__description">Already have an account? | Log In/p>
                                     </div>
 
 
