6c5e5aa21314c0c47d644153df7c33f42c6ad031
pyramakerz-soft <pyramakerzgit@gmail.com>
2025-03-06 12:19:36 +0200

    left

--- resources/views/include/admin-topbar.blade.php
+++ resources/views/include/admin-topbar.blade.php
@@ -10,27 +10,27 @@
 
 
                             </div>
-                            
-                        </div>
-                        <div class="dashboardarea__left__content">
-                            {{-- <h4>{{ $student->name }}</h4> --}}
-                            <h4>{{ Auth::guard('admin')->user()->name ?? 'Admin' }}</h4>
-                        </div>
-                        @if(Auth::guard('admin')->user()->roles[0]->name == 'admin')
-                                    <div class="dashboardarea__right">
-                                        <div class="dashboardarea__right__button">
-                                            <a class="default__button" href="{{route('courses.create')}}">Create a New Course
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                                    class="feather feather-arrow-right">
-                                                    <line x1="5" y1="12" x2="19" y2="12">
-                                                    </line>
-                                                    <polyline points="12 5 19 12 12 19"></polyline>
-                                                </svg></a>
+                            <div class="dashboardarea__left__content">
+                                {{-- <h4>{{ $student->name }}</h4> --}}
+                                <h4>{{ Auth::guard('admin')->user()->name ?? 'Admin' }}</h4>
+                            </div>
+                            @if(Auth::guard('admin')->user()->roles[0]->name == 'admin')
+                                        <div class="dashboardarea__right">
+                                            <div class="dashboardarea__right__button">
+                                                <a class="default__button" href="{{route('courses.create')}}">Create a New Course
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                        viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                        class="feather feather-arrow-right">
+                                                        <line x1="5" y1="12" x2="19" y2="12">
+                                                        </line>
+                                                        <polyline points="12 5 19 12 12 19"></polyline>
+                                                    </svg></a>
+                                            </div>
                                         </div>
-                                    </div>
-@endif
+    @endif
+                        </div>
+                        
                         {{-- <div class="dashboardarea__right">
                             <div class="dashboardarea__right__button">
                                 <a class="default__button" href="{{ route('courses.all') }}">Enroll A New
